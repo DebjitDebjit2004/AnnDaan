39f3f4a7c6a7ce9cc3ed9cdfd22daa8705665be9
DebjitDebjit2004 <dasdebjit1947@gmail.com>
2025-04-20 16:02:31 +0530

    Normal User Controller Ready

--- Backend/server.js
+++ Backend/server.js
@@ -2,14 +2,23 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 const app = express();
 
 // File Requires
 import connectDB from './Config/database.config.js';
+import normalUserRoutes from './Routes/normal.user.routes.js';
 
 const port = process.env.PORT || 3000;
 
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
+// Routes
+app.use('/anndaan/user/normal', normalUserRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
